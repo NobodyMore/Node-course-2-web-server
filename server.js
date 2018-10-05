bd89a0ae3f6badc2b01f68d4223f3a1f53fe87f7
m.a.daneshvar <m.amin.daneshvar@gmail.com>
2018-10-06 01:00:03 +0330

    start Real thing

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+var bodyParser = require('body-parser');
+
+
 
 
 const port=process.env.PORT||3100;
@@ -9,10 +12,13 @@ var app=express();
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use((req,res,next)=>{
   var now=new Date().toString();
-  var log=('${now}:'+now+'--'+req.method+'--'+req.url);
+  var log=('${now}:'+now+'--'+req.method+'--'+req.url+JSON.stringify(req.body));
+
   console.log(log);
   fs.appendFile('server.log',log,(err)=>{
     if(err){
@@ -22,39 +28,94 @@ app.use((req,res,next)=>{
    next();
 });
 
+var sortByScore=()=>{
+var scores=[];
+var scoreString=fs.readFileSync('scores-data')
+scores=JSON.parse(scoreString);
+scores=scores.sort(function(obj1, obj2) {
+	// Ascending: first age less than the previous
+	return obj2.score-obj1.score;
+});
+fs.writeFileSync('scores-data',JSON.stringify(scores))
+};
+
+var seeScoresFromStart=(number)=>{
+var rscores=[];
+var scores=[];
+var scoreString=fs.readFileSync('scores-data');
+scores=JSON.parse(scoreString);
+for(var i=0;i<number;i++){
+  rscores.push(scores[i]);
+}
+for(var i=0;i<number;i++){
+  console.log(rscores[i]);
+}
+};
+
+
+var seeScoresByName=(name)=>{
+  var scores=[];
+  var scoreString=fs.readFileSync('scores-data')
+  scores=JSON.parse(scoreString);
+  for(var i=0;i<scores.length;i++){
+    if(scores[i].name==name){
+      console.log(scores[i]);
+      break;
+    }
+  }
+};
+
+
+var addScore=(level,score,name)=>{
+var scores=[];
+var score={
+  name,
+  score
+};
+
+var scoreString=fs.readFileSync('scores-data');
+scores=JSON.parse(scoreString);
+scores.push(score);
+fs.writeFileSync('scores-data',JSON.stringify(scores))
+sortByScore();
+};
+
+var deleteScores=()=>{
+var scores=[];
+
+fs.writeFileSync('scores-data',JSON.stringify(scores))
+};
+
+
 
 app.use(express.static(__dirname+'/public'));
 
-hbs.registerHelper('getCurrentYear',()=>{
-  return new Date().getFullYear()
-});
 
-hbs.registerHelper('screamIt',(text)=>{
-  return text.toUpperCase();
-});
 
-app.get('/',(req,res) =>{
-res.render('homePage.hbs',{
-  pageTitle: 'Home Page',
-  WelcomeMassage: 'Welcome to my first ExpressJs',
-  currentYear: new Date().getFullYear()
+
+
+app.post('/',(req,res) =>{
+addScore(req.body.level,req.body.score,req.body.name);
 
 });
+
+
+app.delete('/',(req,res) =>{
+deleteScores();
 });
 
-app.get('/about',(req,res)=>{
-res.render('about.hbs',{
-  pageTitle: 'AboutT Page',
-  currentYear: new Date().getFullYear()
-});
+
+app.get('/top',(req,res) =>{
+seeScoresFromStart(1);
+
 });
 
-// /bad - send back json data with errorMassage
-app.get('/bad',(req,res)=>{
-res.send({
-  errorMassage:'unable to handle request'
+app.post('/name',(req,res) =>{
+  seeScoresByName(req.body.name);
+
 });
-});
+
+
 
 app.listen(port,()=>{
   console.log('server is up on port '+port);
