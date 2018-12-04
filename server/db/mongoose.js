8a78589ad0da53fdafba74b81715cdec21000ce1
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2018-12-04 15:49:57 +0330

    Update dependencies Remove not used dependencies

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose')
 
 let uri = 'mongodb://localhost:27017/Users'
 let options = {
-  useMongoClient: true,
   user: 'root',
   pass: 'Ha021mid',
   authSource: 'admin',
-  promiseLibrary: global.Promise
+  promiseLibrary: global.Promise,
+  useNewUrlParser: true
 }
 
 mongoose.connect(uri, options)
 // let db = mongoose.connection
 // db.on('error', console.error.bind(console, 'connection error:'))
 // db.once('open', function () {
-//   console.log('database connected!')
+//   console.log('database connected!')de
 // })
 module.exports = {mongoose}
